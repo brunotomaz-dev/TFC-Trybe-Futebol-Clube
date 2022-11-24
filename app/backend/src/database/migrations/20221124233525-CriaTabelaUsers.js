1a20a7ee6a0de74c450a307cff5c1a8ca62153b1
brunotomaz-dev <tomaz.bruno@gmail.com>
2022-11-24 20:42:35 -0300

    iniciando requisito 1

---
+++ app/backend/src/database/migrations/20221124233525-CriaTabelaUsers.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', { 
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};
