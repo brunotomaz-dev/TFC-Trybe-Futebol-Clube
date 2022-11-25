ee7028cc05f48aefd55a1ba140f633bac5ac5b20
brunotomaz-dev <tomaz.bruno@gmail.com>
2022-11-24 21:38:16 -0300

    requisito 1

--- app/backend/src/database/migrations/20221124233525-CriaTabelaUsers.js
+++ app/backend/src/database/migrations/20221124233525-CriaTabelaUsers.js
@@ -9,6 +9,21 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
+      username: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      role: {
+        type: Sequelize.STRING  
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      password: {       
+        allowNull: false,
+        type: Sequelize.STRING
+      },
     });
   },
 
