99fe0a16be15b25c881e7395450106df694010fe
brunotomaz-dev <tomaz.bruno@gmail.com>
2023-06-05 13:45:35 -0300

    ajustando port

--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import App from './app';
 
-const PORT = `0.0.0.0:${Number(process.env.PORT)}`;
+const PORT = `0.0.0.0:${Number(process.env.PORT) || 3001}`;
 //const PORT = 3001;
 
 new App().start(PORT);
