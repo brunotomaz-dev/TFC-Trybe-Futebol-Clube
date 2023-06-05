43c014c2bb128173f16950a98cb4d9707327e081
brunotomaz-dev <tomaz.bruno@gmail.com>
2023-06-05 14:06:31 -0300

    ajustando port

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -7,11 +7,6 @@ const config: Options = {
   database: process.env.MYSQLDATABASE || 'TRYBE_FUTEBOL_CLUBE',
   host: process.env.MYSQLHOST,
   port: Number(process.env.MYSQLPORT),
-  dialect: 'mysql',
-  dialectOptions: {
-    timezone: 'Z',
-  },
-  logging: false,
 }
 
 module.exports = {
