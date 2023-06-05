adb872fec1b388d717eb15f70208238c59c74e4f
brunotomaz-dev <tomaz.bruno@gmail.com>
2023-06-05 12:26:37 -0300

    configurado para roder localmente com docker

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -4,14 +4,14 @@ import { Options } from 'sequelize';
 const config: Options = {
   username: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
+  database: process.env.MYSQLDATABASE || 'TRYBE_FUTEBOL_CLUBE',
   host: process.env.MYSQLHOST,
   port: Number(process.env.MYSQLPORT),
   dialect: 'mysql',
+  dialectOptions: {
+    timezone: 'Z',
+  },
+  logging: false,
 }
 
-module.exports = {
-  development: config,
-  test: config,
-  production: config,
-};
+module.exports = config;
