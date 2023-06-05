ecda80c7b7313ad39207fff7f14dbb811e706803
brunotomaz-dev <tomaz.bruno@gmail.com>
2023-06-05 15:47:15 -0300

    ajustando package.json do frontend para tentar evitar erro do deploy

--- app/backend/src/database/models/index.ts
+++ app/backend/src/database/models/index.ts
@@ -2,10 +2,6 @@ import { Sequelize } from 'sequelize';
 // import * as config from '../config/database';
 const config = require("../config/database")["development"];
 
-
-
-// const sequelize = new Sequelize(config)
-
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 
