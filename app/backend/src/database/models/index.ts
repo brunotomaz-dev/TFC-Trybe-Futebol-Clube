862baea4509e6158191cb5d9b7e6213269ad2c8b
brunotomaz-dev <tomaz.bruno@gmail.com>
2023-06-05 14:42:45 -0300

    ajustando port

--- app/backend/src/database/models/index.ts
+++ app/backend/src/database/models/index.ts
@@ -1,6 +1,12 @@
+import "dotenv.config";
 import { Sequelize } from 'sequelize';
-import * as config from '../config/database';
+// import * as config from '../config/database';
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.js')[env];
 
-const sequelize = new Sequelize(config)
+// const sequelize = new Sequelize(config)
+
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
 
 export default sequelize;
