418bbac6b97aacf3fee5f81fc7615a71b6bb06b3
brunotomaz-dev <tomaz.bruno@gmail.com>
2023-06-05 15:11:36 -0300

    ajustando sequelize config

--- app/backend/src/database/models/index.ts
+++ app/backend/src/database/models/index.ts
@@ -1,12 +1,17 @@
 import "dotenv/config";
 import { Sequelize } from 'sequelize';
 // import * as config from '../config/database';
+const process = require('process');
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '../config/database.js')[env];
 
 // const sequelize = new Sequelize(config)
 
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
-
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 export default sequelize;
